6d3d3a575ee5b90db53b27aad0cb7c7a9346ef78
Jan Mosig <jan.mosig@itemis.de>
2017-09-08 11:19:49 +0200

    RED: Constructor is missing.

---
+++ ringbuffer/pattern_solution/src/test/java/de/itemis/mosig/ringbuffer/CircularBufferTest.java
@@ -0,0 +1,11 @@
+package de.itemis.mosig.ringbuffer;
+
+import org.junit.Test;
+
+public class CircularBufferTest {
+
+	@Test
+	public void constructorShouldSetProvidedSize() {
+		new CircularBuffer();
+	}
+}
