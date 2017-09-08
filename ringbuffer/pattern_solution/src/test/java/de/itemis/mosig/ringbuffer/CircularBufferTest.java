c4fcde30840e3dc71f5f52d88768cbcd92da6ce9
Jan Mosig <jan.mosig@itemis.de>
2017-09-08 11:34:24 +0200

    RED: Test now uses random nbr.

--- ringbuffer/pattern_solution/src/test/java/de/itemis/mosig/ringbuffer/CircularBufferTest.java
+++ ringbuffer/pattern_solution/src/test/java/de/itemis/mosig/ringbuffer/CircularBufferTest.java
@@ -1,13 +1,16 @@
 package de.itemis.mosig.ringbuffer;
 
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CircularBufferTest {
 
 	@Test
-	public void constructorShouldSetProvidedSize() {
-		CircularBuffer underTest = new CircularBuffer(3);
-		Assert.assertEquals(3, underTest.size());
+	public void whenPassingRandomNbrToTheConstructorSizeShouldBeThatNbr() {
+		int expectedSize = new Random().nextInt(Integer.MAX_VALUE);
+		CircularBuffer underTest = new CircularBuffer(expectedSize);
+		Assert.assertEquals(expectedSize, underTest.size());
 	}
 }
