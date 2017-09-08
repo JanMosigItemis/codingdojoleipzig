e462e77e0c7e54601e328297ecc699ade3c4eb9f
Jan Mosig <jan.mosig@itemis.de>
2017-09-08 11:29:24 +0200

    RED: Test uses non existing size method.

--- ringbuffer/pattern_solution/src/test/java/de/itemis/mosig/ringbuffer/CircularBufferTest.java
+++ ringbuffer/pattern_solution/src/test/java/de/itemis/mosig/ringbuffer/CircularBufferTest.java
@@ -1,11 +1,13 @@
 package de.itemis.mosig.ringbuffer;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CircularBufferTest {
 
 	@Test
 	public void constructorShouldSetProvidedSize() {
-		new CircularBuffer(3);
+		CircularBuffer underTest = new CircularBuffer(3);
+		Assert.assertEquals(3, underTest.size());
 	}
 }
