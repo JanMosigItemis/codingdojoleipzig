f3340efadf108e0d158e131eb6761d3db5cd48a2
Jan Mosig <jan.mosig@itemis.de>
2017-09-08 11:43:44 +0200

    GREEN: Size now returns specified size.

--- ringbuffer/pattern_solution/src/main/java/de/itemis/mosig/ringbuffer/CircularBuffer.java
+++ ringbuffer/pattern_solution/src/main/java/de/itemis/mosig/ringbuffer/CircularBuffer.java
@@ -2,10 +2,13 @@ package de.itemis.mosig.ringbuffer;
 
 public class CircularBuffer {
 
+	private final int size;
+
 	public CircularBuffer(int size) {
+		this.size = size;
 	}
 
 	public int size() {
-		return 3;
+		return size;
 	}
 }
