12d1a242acbe3cace850e10958db96240ef45b59
rohkohl <erikrohkohl@googlemail.com>
2017-10-10 16:00:00 +0200

    added TODOs to roman converter

--- junit5/src/test/java/de/itemis/mosig/dojo/junit5/RomanConverterTest.java
+++ junit5/src/test/java/de/itemis/mosig/dojo/junit5/RomanConverterTest.java
@@ -9,7 +9,7 @@ public class RomanConverterTest {
 
 	private RomanConverter underTest;
 
-	// TODO annotate this method, thus junit-5 executes it before each test case
+	// TODO annotate this method, such that junit-5 executes it before junit runs each test case
 	public void setUp() {
 		this.underTest = new RomanConverter();
 	}
@@ -27,78 +27,73 @@ public class RomanConverterTest {
 	}
 
 	// TODO prevent this test case to be executed
-	@Test
 	public void shouldConvertFiveToV() { assert(false); }
 
-	// TODO add custom assertion message as string
-	@Test
+	// TODO add a custom assertion message as string
 	public void shouldConvertTenToX() {
 		String result = underTest.convert(10);
 		Assert.assertEquals("X", result);
 	}
 
-	// TODO add custom assertion message as lambda expression
-	@Test
+	// TODO add a custom assertion message as lambda expression
 	public void shouldConvertFiftyToL() {
 		String result = underTest.convert(50);
 		Assert.assertEquals("L", result);
 	}
 
-	@Test
+	// TODO assert that two results of the same input do not share a common reference
 	public void shouldConvertOneHundredToC() {
 		String result = underTest.convert(100);
 		Assert.assertEquals("C", result);
 	}
 
-	@Test
+	// TODO group the next three assertions into one assertion
 	public void shouldConvertFiveHundredToD() {
 		String result = underTest.convert(500);
 		Assert.assertEquals("D", result);
 	}
 
-	@Test
 	public void shouldConvertOneThousandToM() {
 		String result = underTest.convert(1000);
 		Assert.assertEquals("M", result);
 	}
 
-	@Test
 	public void shouldConvert6ToVI() {
 		String result = underTest.convert(6);
 		Assert.assertEquals("VI", result);
 	}
 
-	@Test
+	// TODO add a true assumption
 	public void shouldConvert16ToXVI() {
 		String result = underTest.convert(16);
 		Assert.assertEquals("XVI", result);
 	}
 
-	@Test
+	// TODO add a false assumption
 	public void shouldConvert66ToLXVI() {
 		String result = underTest.convert(66);
 		Assert.assertEquals("LXVI", result);
 	}
 
-	@Test
+	// TODO assume that this test runs in CI environment and add assertion
 	public void shouldConvert166ToCLXVI() {
 		String result = underTest.convert(166);
 		Assert.assertEquals("CLXVI", result);
 	}
 
-	@Test
+	// TODO mark this test case with a custom tag and exclude it in maven-surefire-plugin
 	public void shouldConvert666ToDCLXVI() {
 		String result = underTest.convert(666);
 		Assert.assertEquals("DCLXVI", result);
 	}
 
-	@Test
+	// TODO embed this test in a nested class
 	public void shouldConvert1666ToMDCLXVI() {
 		String result = underTest.convert(1666);
 		Assert.assertEquals("MDCLXVI", result);
 	}
 
-	@Test
+	// TODO execute this test ten times
 	public void shouldConvert0ToNothing() {
 		String result = underTest.convert(0);
 		Assert.assertEquals("", result);
