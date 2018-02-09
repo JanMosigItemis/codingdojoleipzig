186b1130f0dec7acdb023bb724a3c5bf5f6d00dc
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 17:54:59 +0100

    RED: Pages converter must accept textfilereader.

--- racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/HtmlPagesConverterTest.java
+++ racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/HtmlPagesConverterTest.java
@@ -1,16 +1,13 @@
 package de.itemis.mosig.racecar.textconv;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 import java.io.IOException;
-
-import org.junit.Test;
 
 
 public class HtmlPagesConverterTest {
     @Test
-    public void foo() throws IOException {
-        HtmlPagesConverter converter = new HtmlPagesConverter("foo");
-        assertEquals("fixme", converter.getFilename());
+    public void shouldReturnFullPageIfThereAreNoLineBreaks() throws IOException {
+        HtmlPagesConverter converter = new HtmlPagesConverter(new TextFileReader(null));
     }
 }
