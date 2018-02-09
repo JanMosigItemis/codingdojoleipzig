aba979f5458f5e16d8214e4fcbdfbcd5f1fef6d6
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 15:14:17 +0100

    RED: Expected test result is generated randomly.

--- racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/TextFileReaderTest.java
+++ racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/TextFileReaderTest.java
@@ -1,15 +1,27 @@
 package de.itemis.mosig.racecar.textconv;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
 
 public class TextFileReaderTest {
 
     @Test
-    public void shouldReadContentsOfExistingFile() {
-        TextFileReader underTest = new TextFileReader(Paths.get(""));
+    public void shouldReadContentsOfExistingFile() throws IOException {
+        Path tmpFilePath = Files.createTempFile(this.getClass().getSimpleName(),"txt");
+        String expectedContent = new String(new byte[]{(byte) (char) (new Random().nextInt(40) + 20)}, StandardCharsets.UTF_8);
+        Files.write(tmpFilePath, expectedContent.getBytes(StandardCharsets.UTF_8));
+
+        TextFileReader underTest = new TextFileReader(tmpFilePath);
 
         String result = underTest.contents();
+
+        Assert.assertEquals(expectedContent, result);
     }
 }
