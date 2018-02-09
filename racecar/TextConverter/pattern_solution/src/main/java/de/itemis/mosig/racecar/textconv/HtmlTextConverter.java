427b742852eab7c2bfc2c29bc0c4b355cdc07d32
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 17:34:11 +0100

    GREEN: Converter does now accept file reader.

--- racecar/TextConverter/pattern_solution/src/main/java/de/itemis/mosig/racecar/textconv/HtmlTextConverter.java
+++ racecar/TextConverter/pattern_solution/src/main/java/de/itemis/mosig/racecar/textconv/HtmlTextConverter.java
@@ -1,15 +1,8 @@
 package de.itemis.mosig.racecar.textconv;
 
-import com.google.common.collect.Streams;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
 public class HtmlTextConverter {
-    public HtmlTextConverter() {
+
+    public HtmlTextConverter(TextFileReader reader) {
 
     }
 
