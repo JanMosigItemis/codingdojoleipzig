428674b7ef46b32fb15d76493250ef76bc5582cb
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 16:17:08 +0100

    REFACTOR: Removed unnecessary code.

--- racecar/TextConverter/pattern_solution/src/main/java/de/itemis/mosig/racecar/textconv/HtmlTextConverter.java
+++ racecar/TextConverter/pattern_solution/src/main/java/de/itemis/mosig/racecar/textconv/HtmlTextConverter.java
@@ -5,28 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-public class HtmlTextConverter
-{
-    private String fullFilenameWithPath;
-
+public class HtmlTextConverter {
     public HtmlTextConverter() {
 
     }
 
-    public String convertToHtml(String input) throws IOException{
-
-	    String line = null;
-	    String html = "&lt;&gt;";
-	    while (line != null)
-	    {
-	    	html += StringEscapeUtils.escapeHtml(line);
-	        html += "<br />";
-	    }
-	    return html;
-
+    public String convertToHtml(String input) {
+	    return "&lt;&gt;";
     }
-
-	public String getFilename() {
-		return this.fullFilenameWithPath;
-	}
 }
