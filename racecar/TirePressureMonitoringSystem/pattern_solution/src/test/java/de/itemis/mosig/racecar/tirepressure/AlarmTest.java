76947cb7b47df9c4363342e205b06da39cd8d7f0
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 11:51:04 +0100

    RED: New test for lower interval bound
    Intentionally made red to see if it is working.

--- racecar/TirePressureMonitoringSystem/pattern_solution/src/test/java/de/itemis/mosig/racecar/tirepressure/AlarmTest.java
+++ racecar/TirePressureMonitoringSystem/pattern_solution/src/test/java/de/itemis/mosig/racecar/tirepressure/AlarmTest.java
@@ -2,6 +2,7 @@ package de.itemis.mosig.racecar.tirepressure;
 
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,5 +12,15 @@ public class AlarmTest {
     public void alarmShouldBeOffAfterConstruction() {
         Alarm alarm = new Alarm(new Sensor());
         assertEquals(false, alarm.isAlarmOn());
+    }
+
+    @Test
+    public void alarmShouldGoOffIfPressureIsBelowInterval() {
+        Sensor mockedSensor = Mockito.mock(Sensor.class);
+        Mockito.when(mockedSensor.popNextPressurePsiValue()).thenReturn(17d);
+
+        Alarm alarm = new Alarm(mockedSensor);
+        alarm.check();
+        assertEquals(true, alarm.isAlarmOn());
     }
 }
