389794b8dfca9ccaae0b0317e14325097ab6014e
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 14:54:10 +0100

    RED: Introduced new TextFileReaderTest

---
+++ racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/TextFileReaderTest.java
@@ -0,0 +1,11 @@
+package de.itemis.mosig.racecar.textconv;
+
+import org.junit.Test;
+
+public class TextFileReaderTest {
+
+    @Test
+    public void shouldReadContentsOfExistingFile() {
+        new TextFileReader();
+    }
+}
