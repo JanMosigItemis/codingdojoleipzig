5e1df4da7883856de1611536f896a29940b592cc
Jan Mosig <jan.mosig@itemis.de>
2018-02-09 15:22:26 +0100

    REFACTOR: Test does now proper tmpFile creation and deletion.

--- racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/TextFileReaderTest.java
+++ racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/TextFileReaderTest.java
@@ -1,6 +1,8 @@
 package de.itemis.mosig.racecar.textconv;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -12,10 +14,30 @@ import java.util.Random;
 
 public class TextFileReaderTest {
 
+    private Path tmpFilePath;
+
+    @Before
+    public void setUp() {
+        try {
+            tmpFilePath = Files.createTempFile(this.getClass().getSimpleName(),"txt");
+        } catch (IOException e) {
+            Assert.fail("Could not create temporary test file: " + e.getMessage());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        if (tmpFilePath != null) {
+            try {
+                Files.deleteIfExists(tmpFilePath);
+            } catch (IOException e) {
+                System.err.println("WARNING: Could not delete temporary test file: " + e.getMessage());
+            }
+        }
+    }
     @Test
     public void shouldReadContentsOfExistingFile() throws IOException {
-        Path tmpFilePath = Files.createTempFile(this.getClass().getSimpleName(),"txt");
-        String expectedContent = new String(new byte[]{(byte) (char) (new Random().nextInt(40) + 20)}, StandardCharsets.UTF_8);
+        String expectedContent = generateRandomString();
         Files.write(tmpFilePath, expectedContent.getBytes(StandardCharsets.UTF_8));
 
         TextFileReader underTest = new TextFileReader(tmpFilePath);
@@ -23,5 +45,13 @@ public class TextFileReaderTest {
         String result = underTest.contents();
 
         Assert.assertEquals(expectedContent, result);
+    }
+
+    /*
+     * ##### start private helper code
+     */
+
+    private String generateRandomString() {
+        return new String(new byte[]{(byte) (char) (new Random().nextInt(40) + 20)}, StandardCharsets.UTF_8);
     }
 }
