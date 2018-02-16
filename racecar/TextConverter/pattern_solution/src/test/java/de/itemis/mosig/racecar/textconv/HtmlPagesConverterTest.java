aefb398cba33f07437c14d27cd8ca91af89f3aa1
Jan Mosig <jan.mosig@itemis.de>
2018-02-16 11:33:09 +0100

    Beginning blackboxtesting of HtmlPagesConverter

--- racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/HtmlPagesConverterTest.java
+++ racecar/TextConverter/pattern_solution/src/test/java/de/itemis/mosig/racecar/textconv/HtmlPagesConverterTest.java
@@ -1,13 +1,45 @@
 package de.itemis.mosig.racecar.textconv;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 
 public class HtmlPagesConverterTest {
+
+    private Path pathToFileWithNoPageBreaks;
+
+    @Before
+    public void setUp() {
+        try (InputStream srcFileStream = getClass().getResourceAsStream("/no_page_breaks.txt")) {
+            Assert.assertNotNull(srcFileStream);
+            pathToFileWithNoPageBreaks = Files.createTempFile(this.getClass().getSimpleName(), "txt");
+            Files.copy(srcFileStream, pathToFileWithNoPageBreaks, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            Assert.fail("Could not copy resource file to temporary location - " + e.getClass().getSimpleName() + ": " + e.getMessage());
+        }
+    }
+
+    @After
+    public void tearDown() {
+        if (pathToFileWithNoPageBreaks != null) {
+            try {
+                Files.deleteIfExists(pathToFileWithNoPageBreaks);
+            } catch (IOException e) {
+                System.err.println("WARN - Could not remove temporary file: " + pathToFileWithNoPageBreaks);
+            }
+        }
+    }
+
     @Test
-    public void shouldReturnFullPageIfThereAreNoLineBreaks() throws IOException {
-        HtmlPagesConverter converter = new HtmlPagesConverter(new TextFileReader(null));
+    public void shouldReturnFullFileContentsIfThereAreNoPageBreaks() throws IOException {
+        HtmlPagesConverter converter = new HtmlPagesConverter(pathToFileWithNoPageBreaks.toString());
     }
 }
